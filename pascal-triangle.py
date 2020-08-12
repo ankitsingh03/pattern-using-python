2caf7311cc947060be5d894ef021d33adb8d1606
ankit singh kushwaha <65447864+ankitsingh03@users.noreply.github.com>
2020-08-12 23:13:43 +0530

    Update pascal-triangle.py

--- pascal-triangle.py
+++ pascal-triangle.py
@@ -13,12 +13,16 @@ for i in range(n):
     inner_lst = []
     # use for number of column
     for j in range(i+1):
+        #insert 1 on first and last position
         if j == 0 or j == i:
             inner_lst.append(1)
+        #insert middle values
         else:
             inner_lst.append(lst[i-1][j-1] + lst[i-1][j])
     lst.append(inner_lst)
 
 for i in range(n):
+    #use for space print
     print(" " * (n-i-1), end="")
+    #unpack the list
     print(*lst[i])
